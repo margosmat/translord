d3f280c6edfac6d24a5d7d6abaf173c58cd1067d
Mateusz Margos <mateuszmargos@gmail.com>
2024-04-07 14:59:09 +0200

    grouped translation object refactor

--- translord/Models/Translation.cs
+++ translord/Models/Translation.cs
@@ -7,4 +7,14 @@ public class Translation
     public required string Key { get; set; }
     public required string Value { get; set; }
     public Language Language { get; set; }
+
+    public Translation Copy()
+    {
+        return new Translation
+        {
+            Key = Key,
+            Value = Value,
+            Language = Language
+        };
+    }
 }
