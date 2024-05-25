226fedcb3469babeeecac3d375cd863980c33758
Mateusz Margos <mateuszmargos@gmail.com>
2024-05-25 22:17:34 +0200

    translord.Manager package prepared for nuget, cleanup

--- translord.Manager/Models/GroupedTranslations.cs
+++ translord.Manager/Models/GroupedTranslations.cs
@@ -2,9 +2,9 @@ using translord.Models;
 
 namespace translord.Manager.Models;
 
-public class GroupedTranslations(int size)
+public class GroupedTranslations
 {
     public required string Key { get; set; }
 
-    public List<Translation> Translations { get; set; } = new List<Translation>();
+    public List<Translation> Translations { get; init; } = [];
 }
