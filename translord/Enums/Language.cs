f0e3469a158166f57f279c46b3c0cf984c9a2c39
Mateusz Margos <mateuszmargos@gmail.com>
2024-06-01 23:11:58 +0200

    Language enum method added, small refactor

--- translord/Enums/Language.cs
+++ translord/Enums/Language.cs
@@ -39,91 +39,129 @@ public static class LanguageExtensions
 {
     public static string GetIsoCode(this Language language)
     {
-        switch (language)
+        return language switch
         {
-            case Language.EnglishBritish: return "en-gb";
-            case Language.Polish: return "pl";
-            case Language.German: return "de";
-            case Language.French: return "fr";
-            case Language.Spanish: return "es";
-            case Language.Italian: return "it";
-            case Language.Ukrainian: return "uk";
-            case Language.Czech: return "cs";
-            case Language.Japanese: return "ja";
-            case Language.Bulgarian: return "bg";
-            case Language.Danish: return "da";
-            case Language.Greek: return "el";
-            case Language.EnglishAmerican: return "en-us";
-            case Language.Estonian: return "et";
-            case Language.Finnish: return "fi";
-            case Language.Hungarian: return "hu";
-            case Language.Indonesian: return "id";
-            case Language.Korean: return "ko";
-            case Language.Lithuanian: return "lt";
-            case Language.Latvian: return "lv";
-            case Language.Norwegian: return "nb";
-            case Language.Dutch: return "nl";
-            case Language.PortugueseBrazilian: return "pt-br";
-            case Language.PortugueseEuropean: return "pt-pt";
-            case Language.Romanian: return "ro";
-            case Language.Russian: return "ru";
-            case Language.Slovak: return "sk";
-            case Language.Slovenian: return "sl";
-            case Language.Swedish: return "sv";
-            case Language.Turkish: return "tr";
-            case Language.ChineseSimplified: return "zh";
-            default: throw new NotImplementedException("Language is not implemented yet.");
-        }
+            Language.EnglishBritish => "en-gb",
+            Language.Polish => "pl",
+            Language.German => "de",
+            Language.French => "fr",
+            Language.Spanish => "es",
+            Language.Italian => "it",
+            Language.Ukrainian => "uk",
+            Language.Czech => "cs",
+            Language.Japanese => "ja",
+            Language.Bulgarian => "bg",
+            Language.Danish => "da",
+            Language.Greek => "el",
+            Language.EnglishAmerican => "en-us",
+            Language.Estonian => "et",
+            Language.Finnish => "fi",
+            Language.Hungarian => "hu",
+            Language.Indonesian => "id",
+            Language.Korean => "ko",
+            Language.Lithuanian => "lt",
+            Language.Latvian => "lv",
+            Language.Norwegian => "nb",
+            Language.Dutch => "nl",
+            Language.PortugueseBrazilian => "pt-br",
+            Language.PortugueseEuropean => "pt-pt",
+            Language.Romanian => "ro",
+            Language.Russian => "ru",
+            Language.Slovak => "sk",
+            Language.Slovenian => "sl",
+            Language.Swedish => "sv",
+            Language.Turkish => "tr",
+            Language.ChineseSimplified => "zh",
+            _ => throw new NotImplementedException("Language is not implemented yet.")
+        };
+    }
+    
+    
+    public static Language FromIsoCode(this string languageIsoCode)
+    {
+        return languageIsoCode switch
+        {
+            "en-gb" => Language.EnglishBritish,
+            "pl" => Language.Polish,
+            "de" => Language.German,
+            "fr" => Language.French,
+            "es" => Language.Spanish,
+            "it" => Language.Italian,
+            "uk" => Language.Ukrainian,
+            "cs" => Language.Czech,
+            "ja" => Language.Japanese,
+            "bg" => Language.Bulgarian,
+            "da" => Language.Danish,
+            "el" => Language.Greek,
+            "en-us" => Language.EnglishAmerican,
+            "et" => Language.Estonian,
+            "fi" => Language.Finnish,
+            "hu" => Language.Hungarian,
+            "id" => Language.Indonesian,
+            "ko" => Language.Korean,
+            "lt" => Language.Lithuanian,
+            "lv" => Language.Latvian,
+            "nb" => Language.Norwegian,
+            "nl" => Language.Dutch,
+            "pt-br" => Language.PortugueseBrazilian,
+            "pt-pt" => Language.PortugueseEuropean,
+            "ro" => Language.Romanian,
+            "ru" => Language.Russian,
+            "sk" => Language.Slovak,
+            "sl" => Language.Slovenian,
+            "sv" => Language.Swedish,
+            "tr" => Language.Turkish,
+            "zh" => Language.ChineseSimplified,
+            _ => throw new NotImplementedException("Language is not implemented yet.")
+        };
     }
     
     public static string GetSourceIsoCode(this Language language)
     {
-        switch (language)
+        return language switch
         {
-            case Language.EnglishBritish:
-            case Language.EnglishAmerican: return "en";
-            case Language.PortugueseBrazilian:
-            case Language.PortugueseEuropean: return "pt";
-            default: return GetIsoCode(language);
-        }
+            Language.EnglishBritish or Language.EnglishAmerican => "en",
+            Language.PortugueseBrazilian or Language.PortugueseEuropean => "pt",
+            _ => GetIsoCode(language)
+        };
     }
     
     public static string GetName(this Language language)
     {
-        switch (language)
+        return language switch
         {
-            case Language.EnglishBritish: return "English (British)";
-            case Language.Polish: return "Polish";
-            case Language.German: return "German";
-            case Language.French: return "French";
-            case Language.Spanish: return "Spanish";
-            case Language.Italian: return "Italian";
-            case Language.Ukrainian: return "Ukrainian";
-            case Language.Czech: return "Czech";
-            case Language.Japanese: return "Japanese";
-            case Language.Bulgarian: return "Bulgarian";
-            case Language.Danish: return "Danish";
-            case Language.Greek: return "Greek";
-            case Language.EnglishAmerican: return "English (American)";
-            case Language.Estonian: return "Estonian";
-            case Language.Finnish: return "Finnish";
-            case Language.Hungarian: return "Hungarian";
-            case Language.Indonesian: return "Indonesian";
-            case Language.Korean: return "Korean";
-            case Language.Lithuanian: return "Lithuanian";
-            case Language.Latvian: return "Latvian";
-            case Language.Norwegian: return "Norwegian";
-            case Language.Dutch: return "Dutch";
-            case Language.PortugueseBrazilian: return "Portuguese (Brazilian)";
-            case Language.PortugueseEuropean: return "Portuguese (European)";
-            case Language.Romanian: return "Romanian";
-            case Language.Russian: return "Russian";
-            case Language.Slovak: return "Slovak";
-            case Language.Slovenian: return "Slovenian";
-            case Language.Swedish: return "Swedish";
-            case Language.Turkish: return "Turkish";
-            case Language.ChineseSimplified: return "Chinese (Simplified)";
-            default: throw new NotImplementedException("Language is not implemented yet.");
-        }
+            Language.EnglishBritish => "English (British)",
+            Language.Polish => "Polish",
+            Language.German => "German",
+            Language.French => "French",
+            Language.Spanish => "Spanish",
+            Language.Italian => "Italian",
+            Language.Ukrainian => "Ukrainian",
+            Language.Czech => "Czech",
+            Language.Japanese => "Japanese",
+            Language.Bulgarian => "Bulgarian",
+            Language.Danish => "Danish",
+            Language.Greek => "Greek",
+            Language.EnglishAmerican => "English (American)",
+            Language.Estonian => "Estonian",
+            Language.Finnish => "Finnish",
+            Language.Hungarian => "Hungarian",
+            Language.Indonesian => "Indonesian",
+            Language.Korean => "Korean",
+            Language.Lithuanian => "Lithuanian",
+            Language.Latvian => "Latvian",
+            Language.Norwegian => "Norwegian",
+            Language.Dutch => "Dutch",
+            Language.PortugueseBrazilian => "Portuguese (Brazilian)",
+            Language.PortugueseEuropean => "Portuguese (European)",
+            Language.Romanian => "Romanian",
+            Language.Russian => "Russian",
+            Language.Slovak => "Slovak",
+            Language.Slovenian => "Slovenian",
+            Language.Swedish => "Swedish",
+            Language.Turkish => "Turkish",
+            Language.ChineseSimplified => "Chinese (Simplified)",
+            _ => throw new NotImplementedException("Language is not implemented yet.")
+        };
     }
 }
